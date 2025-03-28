04322c55bc71b1bee9772a09bc6b3ece2a55de5d
Xi Shunjia <13844195031@163.com>
2025-03-28 19:38:03 +0800

    更改了evaluate函数，使用数据集多线程加载功能

--- NewCode/utils.py
+++ NewCode/utils.py
@@ -105,7 +105,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
     for step, sample_batch in enumerate(data_loader):
         # 数据迁移到设备
         imageleft = sample_batch["left_image"].to(device)
-        imageright = sample_batch["right_image"].to(device)
+        # imageright = sample_batch["right_image"].to(device)
         pos = sample_batch["position"].squeeze().to(device)
         target = sample_batch["hrtf"].squeeze(1)[:, :].to(device)
 
@@ -129,28 +129,26 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
 
 @torch.no_grad()
 def evaluate(model, data_loader, device, epoch):
-    loss_function = torch.nn.CrossEntropyLoss()
- 
     model.eval()
- 
-    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    loss_function = torch.nn.MSELoss()
     accu_loss = torch.zeros(1).to(device)  # 累计损失
  
-    sample_num = 0
     data_loader = tqdm(data_loader, file=sys.stdout)
-    for step, data in enumerate(data_loader):
-        images, labels = data
-        sample_num += images.shape[0]
+    for step, sample_batch in enumerate(data_loader):
+        # 数据迁移到设备
+        imageleft = sample_batch["left_image"].to(device)
+        # imageright = sample_batch["right_image"].to(device)
+        pos = sample_batch["position"].squeeze().to(device)
+        target = sample_batch["hrtf"].squeeze(1)[:, :].to(device)
+
+        # 前向传播
+        #output = model(imageleft, imageright, pos)
+        output = model(imageleft, pos)
+        loss = loss_function(output, target)
+        accu_loss += loss.detach()  # detach() 防止梯度传播
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}".format(
+            epoch, accu_loss.item() / (step + 1)
+        )
  
-        pred = model(images.to(device))
-        pred_classes = torch.max(pred, dim=1)[1]
-        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
- 
-        loss = loss_function(pred, labels.to(device))
-        accu_loss += loss
- 
-        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
-                                                                               accu_loss.item() / (step + 1),
-                                                                               accu_num.item() / sample_num)
- 
-    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+    return accu_loss.item() / (step + 1)
