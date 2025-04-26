5ef83ff1c19eccbad1dc112de4b0147a3f83a0d5
myth <221200023@smail.nju.edu.cn>
2025-04-26 14:04:52 +0800

    实现单频率计算

--- NewCode/cal_LSD_single.py
+++ NewCode/cal_LSD_single.py
@@ -13,7 +13,7 @@ from new_dataset import SonicomDataSet, SingleSubjectDataSet
 
 # from utils import read_split_data, train_one_epoch, evaluate
 
-model_path = "D:\大学\大三下\大创项目\Sonicom--\weights\model-9.pth"
+model_path = "weights\model-9.pth"
 # model_path = "123"
 
 target_index = 100
